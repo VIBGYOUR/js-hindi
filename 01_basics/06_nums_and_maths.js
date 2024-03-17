3e0e2c7a2582871447bd14f2819715056daa9fe7
Parag Kaushik <91472710+VIBGYOUR@users.noreply.github.com>
2024-03-17 14:56:16 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score = 400.789
+const balance = new Number(100)
+// console.log(balance.toString().length,score.toFixed(5));
+
+// const otherNumber = 45.9465343453
+// console.log(otherNumber.toPrecision(1))
+
+const hundred = 100000000
+// console.log(hundred.toLocaleString())
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$  MATHS   $$$$$$$$$$$$$$$$$$$$$$$$
+
+// console.log(Math)
+// console.log(Math.abs(-76))
+// console.log(Math.round(score))
+// console.log(Math.ceil(score))
+// console.log(Math.floor(score))
+// console.log(Math.min(4,3,6,8))
+
+// console.log(Math.round(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.round((Math.random()*(max-min+1))+min))
+
+
