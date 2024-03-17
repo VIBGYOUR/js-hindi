403302af6c3b767eec60580553df2ff1a87d6f50
Parag Kaushik <91472710+VIBGYOUR@users.noreply.github.com>
2024-03-17 06:32:41 +0000

    dataypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(undefined==null);
+// console.log(null==0);
+
+console.log("2"===2);
+console.log("2"==="3");
+console.log("2"==="2");
+
+//different data type comparisons cause improbable results
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive 
+//7 types: String , Number , Boolean , null, undefined, Symbol, BigInt, 
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId);
+// console.log(id);
+// 
+const bigNumber = 1284585346514173745n
+// console.log(typeof bigNumber);
+
+//Reference(Non-primitive)
+//Arrays, Objects, Functions,  
+
+
+const heros = ["shaktiman","naagraj","shakalakaboom"]
+
+  const myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+  console.log("hello World");
+}
+
+console.log(typeof outsideTemp)
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
