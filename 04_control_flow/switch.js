71819ced76696e426e9e96e26cdc64ea415fecd3
Parag Kaushik <91472710+VIBGYOUR@users.noreply.github.com>
2024-03-19 00:27:39 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("Month is januray");
+        break;
+    case "feb":
+        console.log("Month is february");
+        break;
+    case "march":
+        console.log("Month is march");
+        break;
+    case "april":
+        console.log("Month is april");
+        break;
+    default:
+        console.log("Month unknown");
+        break;
+}
